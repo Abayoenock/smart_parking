a950bf036cff6521a837ecc71ae8778a76af3430
Abayo Himbaza  Enock <abayo.h.enock@gmail.com>
2022-12-21 18:05:57 +0200

    Update nodemcu.ino

--- circuit/nodemcu/nodemcu.ino
+++ circuit/nodemcu/nodemcu.ino
@@ -1,7 +1,8 @@
-#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include<SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#include <SoftwareSerial.h>
 SoftwareSerial s(3,1);
 const char* ssid = "Virus";
 const char* password = "mbega123455";
@@ -11,25 +12,26 @@ String serverName = "http://137.184.232.255/smart_parking/data.php";
 void setup() {
   s.begin(9600);
   WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED) {
-  delay(500);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
   }
 }
 
 void loop() {
-    if(s.available( ) > 0){
-      WiFiClient client;
-      HTTPClient http;
-      http.begin(client, serverName);
-      //http.addHeader("Content-Type", "application/json");
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      String httpRequestData = s.readStringUntil('\n');
-      int      httpResponseCode = http.POST(httpRequestData);
-      if (httpResponseCode>0) {
-        s.println(httpResponseCode);
-        String payload = http.getString();
-        s.println(payload);
-      }
-      http.end();
-    }
-  }
+  if (s.available() > 0) {
+  WiFiClient client;
+  HTTPClient http;
+  String httpRequestData = s.readStringUntil('\n');
+  //String httpRequestData = "?card=2456314562";
+  String url = serverName + httpRequestData;
+  http.begin(client, url);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httpCode = http.GET();
+  delay(3000);
+  String payload = http.getString();  // get data from webhost continously
+  s.println(payload);
+  http.end();
+   }
+}
+
